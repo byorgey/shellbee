d66bcfaadeb55193a360f5720c560dc9049d07b8
Brent Yorgey <byorgey@cis.upenn.edu>
2013-06-02 20:34:47 -0400

    start working on a command to list goals

--- Main.hs
+++ Main.hs
@@ -2,41 +2,23 @@
 {-# LANGUAGE TemplateHaskell       #-}
 
 import           Control.Lens
+import           Control.Monad                          (when)
+import           Control.Monad.IO.Class                 (liftIO)
 import qualified Data.ByteString.Char8                  as BS8 (pack)
 import           Data.Default.Class
 import           Data.List
+import           Data.Maybe                             (isNothing)
 import           Network.Beeminder
 import           System.Console.Shell
 import           System.Console.Shell.Backend.Haskeline
 import           System.Console.Shell.ShellMonad
 import           System.Environment
 
-react :: String -> Sh () ()
-react str = shellPutStrLn ("You entered " ++ str ++ "!")
-
-shellbee = initialShellDescription
-  { shellCommands =
-    [ exitCommand "quit"
-    , helpCommand "help"
-    , cmd "foo" fooFunc "Do the foo."
-    ]
-  , commandStyle = OnlyCommands
-  , evaluateFunc = react
-  , greetingText = Just "ShellBee!"
-  }
-
-fooFunc :: Completable Wibble -> File -> Username -> Sh () ()
-fooFunc (Completable s) (File f) (Username u) = shellPutStrLn $ "Your choice of wibble: " ++ s ++ "; file: " ++ f ++ "; username: " ++ u
-
-data Wibble = Wibble
-
-instance Completion Wibble () where
-  complete _ _ str = return $ filter (str `isPrefixOf`) ["hello", "hi", "blue", "black", "green"]
-  completableLabel _ = "wibble"
+type Shellbee = Sh ShellbeeSt
 
 data ShellbeeSt = ShellbeeSt
-  { _token :: Maybe Token
-  , _user  :: Maybe User
+  { _token     :: Maybe Token
+  , _userCache :: Maybe User
   }
   deriving Show
 
@@ -44,12 +26,51 @@ makeLenses ''ShellbeeSt
 
 instance Default ShellbeeSt where
   def = ShellbeeSt
-        { _token = Nothing
-        , _user  = Nothing
+        { _token     = Nothing
+        , _userCache = Nothing
         }
+
+react :: String -> Shellbee ()
+react str = shellPutStrLn ("You entered " ++ str ++ "!")
+
+shellbee :: ShellDescription ShellbeeSt
+shellbee = initialShellDescription
+  { shellCommands =
+    [ exitCommand "quit"
+    , helpCommand "help"
+    , cmd "goals" goalsCmd "Show goals."
+    ]
+  , commandStyle = OnlyCommands
+  , evaluateFunc = react
+  , greetingText = Just "ShellBee!\n"
+  }
+
+goalsCmd :: Shellbee ()
+goalsCmd = do
+  cacheUser
+  with userCache $ \u ->
+    shellPutStrLn $ "Your goals:\n" ++ show u
+
+with l act = use l >>= maybe (return ()) act
+
+cacheUser :: Shellbee ()
+cacheUser = do
+  u <- use userCache
+  when (isNothing u) getUser
+
+getUser :: Shellbee ()
+getUser = do
+  mu <- runBee $ user (def & _GoalsFilter .~ Just Front)
+  userCache .= mu
+
+runBee :: Beeminder a -> Shellbee (Maybe a)
+runBee bee = do
+  mtok <- use token
+  case mtok of
+    Nothing  -> return Nothing
+    Just tok -> liftIO $ runBeeminder tok bee
 
 main = do
   [tok] <- getArgs
   let st = def & token .~ Just (BS8.pack tok)
-  print st
---  runShell shellbee haskelineBackend ()
+  runShell shellbee haskelineBackend st
